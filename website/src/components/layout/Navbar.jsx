68a1fadd14590fa302c1a61ba901b89d869747ab
Aayush Rawat <aayushrawat.dev@gmail.com>
2026-07-22 14:26:00 +0530

    Refactor Navbar component to use NavLink for navigation and improve structure

--- website/src/components/layout/Navbar.jsx
+++ website/src/components/layout/Navbar.jsx
@@ -1,9 +1,26 @@
+import { Link, NavLink } from 'react-router-dom';
+
 const Navbar = () => {
+
+    const navLinks = [
+        {
+            name: "Compare",
+            path: '/',
+        },
+        {
+            name: "Methodology",
+            path: '/methodology',
+        },
+        {
+            name: 'About',
+            path: '/about',
+        }
+    ]
     return (
-        <div className='sticky top-0 z-50 border-b border-slate-200 bg-white backdrop:blur-md'>
+        <header className='sticky top-0 z-50 border-b border-slate-200 bg-white backdrop:blur-md'>
             <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-4 md:px-8 lg:px-12">
-                <a 
-                    href="#"
+                <Link 
+                    to="/"
                     className='flex items-center gap-2'
                 >
                     <i className="ri-graduation-cap-fill text-3xl text-blue-700"></i>
@@ -11,15 +28,25 @@ const Navbar = () => {
                     <span className='text-2xl font-black tracking-tight text-slate-900'>
                         CCS
                     </span>
-                </a>
+                </Link>
 
                 <nav className='hidden items-center gap-8 md:flex'>
-                    <a href="#" className='border-b-2 border-blue-700 pb-1 font-semibold text-blue-700'>Compare</a>
-                    <a href="#" className='font-medium text-slate-600 transition hover:text-blue-700'>Methodology</a>
-                    <a href="#" className='font-medium text-slate-600 transition hover:text-blue-700'>About</a>
+                    {navLinks.map((link) => (
+                        <NavLink
+                            key={link.name} 
+                            to={link.path}
+                            className={({isActive}) => `pb-1 text-sm font-medium transition-colors ${isActive ? 'border-b-2 border-blue-700 text-blue-700' : 'text-slate-600 hover:text-blue-700'}`}
+                            >
+                                {link.name}
+                            </NavLink>
+                    ))}
                 </nav>
+
+                <button className='text-3xl text-slate-700 md:hidden'>
+                    <i className="ri-menu-line"></i>
+                </button>
             </div>  
-        </div>
+        </header>
     )
 }
 
